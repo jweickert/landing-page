56cb5f95d809ca488e66d7c94c4ff2447bbba4ba
Jack Weickert <59937725+jweickert@users.noreply.github.com>
2020-09-25 19:50:44 -0400

    added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,18 @@ import React from 'react';
 import '../styles/Header.css';
 
 function Header() {
-    return (
-        <div className="header">
-          <span className="header-title">
-            HIMS
-          </span>
-          <br />
-          <span className="header-text">
-            Health & Insurance Management System
-          </span>
-        </div>
-      )
+  return (
+      <div className="header">
+        <span className="header-title">
+          <img src="https://i.imgur.com/qqTqVNS.png" />
+        </span>
+        <br />
+        <span className="header-text">
+          Health & Insurance Management System
+        </span>
+      </div>
+    )
 }
+
 
 export default Header

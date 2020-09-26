c517e1fe0b54dff5c6b6298c6a9dfec900543684
Jack Weickert <59937725+jweickert@users.noreply.github.com>
2020-09-25 20:05:06 -0400

    replaced logo with HQ version

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
       <div className="header">
         <span className="header-title">
-          <img src="https://i.imgur.com/qqTqVNS.png" />
+          <img src="https://i.imgur.com/TKJnycm.png" />
         </span>
         <br />
         <span className="header-text">
